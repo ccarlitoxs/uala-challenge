b560d1effc16f4df5caf4cec2ca5cd382c6fc89c
ivancochancod <37812240+ccarlitoxs@users.noreply.github.com>
2025-03-27 20:37:10 -0300

    feat: Add new SVG icon components

---
+++ src/assets/Icons/MenuIcon.tsx
@@ -0,0 +1,33 @@
+import { SVGProps } from 'react';
+
+export const MenuIcon = ({
+  width = '24',
+  height = '24',
+  color = 'currentColor',
+  ...props
+}: SVGProps<SVGSVGElement>) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M3 12H21"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <path d="M3 6H21" stroke={color} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <path
+      d="M3 18H21"
+      stroke={color}
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
---
+++ src/assets/Icons/index.ts
@@ -0,0 +1,4 @@
+export * from './MenuIcon';
+export * from './ChevronIcon';
+export * from './HomeIcon';
+export * from './MetricsIcon';
