b168fa0b0840197d56dc50be01e00d6f085ab279
ivancochancod <37812240+ccarlitoxs@users.noreply.github.com>
2025-03-27 20:36:58 -0300

    feat: Integrate react-router for navigation and update App component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import Home from './screens/Home'
-import { MainLayout } from './components/Layout/MainLayout'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './screens/Home';
+import { MainLayout } from './components/Layout/MainLayout';
 
 function App() {
   return (
-    <MainLayout>
-      <Home />
-    </MainLayout>
-  )
+    <Router>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
